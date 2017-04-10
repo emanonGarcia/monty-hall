9f1f4bfd4139151421aba99af5c17dff71a94141
Luis Garcia <emanon805@VIolas-MacBook-Pro.local>
2017-04-10 15:42:37 -0400

    updates to readme and refactor of main script

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-#Monty Hall
-This program simulates the probabilty of getting a winning choice for the [Monty Hall problem](https://en.wikipedia.org/wiki/Monty_Hall_problem). The program shows that the proabablity of winning is much higher if the contestant switches their choice.
+# Monty Hall
 
-##Getting Started
-After loading the file, excute through the command line from the local directory
+This program simulates the probability of getting a winning choice for the [Monty Hall problem](https://en.wikipedia.org/wiki/Monty_Hall_problem). The program shows that the probability of winning is much higher if the contestant switches their choice.
 
-###Prerequisites
+## Getting Started
+
+After loading the file, execute through the command line from the local directory
+
+### Prerequisites
+
 ```
 pip install -r requirements.txt
 ```
 
-##Authors
+## Authors
+
 * **Luis Garcia** - *Initial work* -[emanonGarcia](https://github.com/emanonGarcia)
--- monty_hall.py
+++ monty_hall.py
@@ -1,21 +1,56 @@
 import random
 
-stay_win = 0
-switch_win = 0
 
-for i in range(1000):
-    doors = ['goat', 'car', 'goat']
-    random.shuffle(doors)
-    players_choice = doors.pop()
-    doors.pop(doors.index('goat'))
-    switch = random.randrange(2)
+def random_decision():
+    stay_win = 0
+    switch_win = 0
+    for i in range(1000):
+        doors = ['goat', 'car', 'goat']
+        random.shuffle(doors)
+        players_choice = doors.pop()
+        doors.pop(doors.index('goat'))
+        switch = random.randrange(2)
 
-    if not switch:
+        if not switch:
+            if players_choice == 'car':
+                stay_win += 1
+        elif switch:
+            if doors[0] == 'car':
+                switch_win += 1
+
+    print("\nBy choosing randomly between stay and switch")
+    print("In {} tries, stay win: {}, switch win: {}".format(i+1, stay_win, switch_win))
+    print('Of total wins: Stay Percentage: {:0.2f}%, Switch Percentage: {:0.2f}%'.format((stay_win / (stay_win + switch_win)) * 100, (switch_win / (stay_win + switch_win)) * 100))
+
+
+def staying():
+    stay_win = 0
+    for i in range(1000):
+        doors = ['goat', 'car', 'goat']
+        random.shuffle(doors)
+        players_choice = doors.pop()
+        doors.pop(doors.index('goat'))
+
         if players_choice == 'car':
-            stay_win += 1
-    elif switch:
-        if doors[0] == 'car':
-            switch_win += 1
+                stay_win += 1
 
-print("In 1000 tries, stay win: {}, switch win: {}".format(stay_win, switch_win))
-print('Stay Percentage: {:0.2f}%, Switch Percentage: {:0.2f}%'.format((stay_win / (stay_win + switch_win)) * 100, (switch_win / (stay_win + switch_win)) * 100))
+    print("Wins by staying: {}/{} ({:0.2f}% chance of winning)".format(stay_win, i+1, (stay_win/1000)*100))
+
+
+def switching():
+    switch_win = 0
+    for i in range(1000):
+        doors = ['goat', 'car', 'goat']
+        random.shuffle(doors)
+        players_choice = doors.pop()
+        doors.pop(doors.index('goat'))
+
+        if players_choice != 'car':
+                switch_win += 1
+
+    print("Wins by switching: {}/{} ({:0.2f}% chance of winning)".format(switch_win, i+1, (switch_win/1000)*100))
+
+
+staying()
+switching()
+random_decision()
